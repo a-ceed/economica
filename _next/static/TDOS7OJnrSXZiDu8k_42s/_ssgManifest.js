4ec56e7cd99b92e265fd89ac66377a9caad5ac31
a-ceed <forjetx@gmail.com>
2026-04-17 19:20:13 +0000

    Deploying to gh-pages from @ a-ceed/economica@3c526082130e0036917af3fb320625db2c8b7913 🚀

---
+++ _next/static/TDOS7OJnrSXZiDu8k_42s/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fhistory","\u002Floansharks","\u002Frgm","\u002Fblog","\u002Fabout","\u002Fapplication2","\u002Fcontacts","\u002Fcreativity","\u002F","\u002Fair","\u002Freality-ecology","\u002Fapplication1","\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
