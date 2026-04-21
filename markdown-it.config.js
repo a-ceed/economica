a9c162e7ff0f9932decf037111665f7f60f139f1
a.sidorov@platformcraft.com <3L@vi!@TacA9DR>
2026-04-21 11:05:18 +0300

    fix video plugin

--- markdown-it.config.js
+++ markdown-it.config.js
@@ -1,6 +1,22 @@
 import markdownit from 'markdown-it';
 import embedEverything from 'markdown-it-embed-everything';
-import RutubeService from './lib/RutubeService.js';
+import VideoServiceBase from 'markdown-it-embed-everything/lib/services/VideoServiceBase.js';
+
+class RutubeService extends VideoServiceBase {
+  getDefaultOptions() {
+    return { width: 720, height: 405 };
+  }
+
+  extractVideoID(reference) {
+    const match = reference.match(/rutube\.ru\/video\/(?:private\/)?([a-f0-9]{32})/i);
+    return match ? match[1] : reference;
+  }
+
+  getVideoUrl(videoID) {
+    const escapedID = this.env.md.utils.escapeHtml(videoID);
+    return `https://rutube.ru/play/embed/${escapedID}`;
+  }
+}
 
 const md = markdownit({
     html: true,
